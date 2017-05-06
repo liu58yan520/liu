0ed487ef81ce14a7d9c09bd9e0ee2ee70d50509c
liu58yan520 <liu58yan520@hotmail.com>
2017-05-06 19:45:20 +0800

    腾信二手车 基本架构

---
+++ test/jsapi.php
@@ -0,0 +1,150 @@
+﻿<?php 
+/*
+http://www.mamicode.com/info-detail-587828.html
+先在公众号平台 微信支付-开发配置写下测试目录，就是本文件所在目录 和我的微信号
+测试目录和正式目录不能一致,以下是四个要用到的参数
+const APPID = 'wx09f41d0fc9acbf14';
+const MCHID = '';
+const KEY = '';
+const APPSECRET = '4f3e6df4c2a6a74c502d2ef017c6e74f';
+APPID 和 APPSECRET都可以在微信后台中找到。
+MCHID 在申请微信支付后发来的邮件中可以找到，KEY 则根据邮件提示
+
+接口API证书 密钥 在商户平台 帐户设置 密码安全 api安全中下载设置
+如果已经申请过 新街口的api证书和密钥不会在随邮件下发
+*/
+
+
+ini_set('date.timezone','Asia/Shanghai');
+//error_reporting(E_ERROR);
+require_once "../lib/WxPay.Api.php";
+require_once "WxPay.JsApiPay.php";
+require_once 'log.php';
+
+//初始化日志
+$logHandler= new CLogFileHandler("../logs/".date('Y-m-d').'.log');
+$log = Log::Init($logHandler, 15);
+
+//打印输出数组信息
+function printf_info($data)
+{
+    foreach($data as $key=>$value){
+        echo "<font color='#00ff55;'>$key</font> : $value <br/>";
+    }
+}
+
+//①、获取用户openid
+$tools = new JsApiPay();
+$openId = $tools->GetOpenid();
+
+/*
+GetOpenid函数中WxPay.JsApiPay.php中  需要自己设置网页授权接口
+http://mp.weixin.qq.com/wiki/17/c0f37d5704f0b64713d5d2c37b468d75.html
+*/
+
+//②、统一下单
+$input = new WxPayUnifiedOrder();
+$input->SetBody("test");
+$input->SetAttach("test");  //这个参数最好去掉 不能有空格
+$input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
+$input->SetTotal_fee("1");
+$input->SetTime_start(date("YmdHis"));
+$input->SetTime_expire(date("YmdHis", time() + 600));
+$input->SetGoods_tag("test");
+$input->SetNotify_url("http://paysdk.weixin.qq.com/example/notify.php");
+//是设置接收支付结果通知的Url 自己写http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']).'/notify.php
+
+$input->SetTrade_type("JSAPI");
+$input->SetOpenid($openId);
+$order = WxPayApi::unifiedOrder($input); //到WxPay.Api.php 中搜索 异步通知url 设置
+echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
+printf_info($order);
+$jsApiParameters = $tools->GetJsApiParameters($order);
+
+//获取共享收货地址js函数参数
+$editAddress = $tools->GetEditAddressParameters();
+
+//③、在支持成功回调通知中处理成功之后的事宜，见 notify.php
+/**
+ * 注意：
+ * 1、当你的回调地址不可访问的时候，回调通知会失败，可以通过查询订单来确认支付是否成功
+ * 2、jsapi支付时需要填入用户openid，WxPay.JsApiPay.php中有获取openid流程 （文档可以参考微信公众平台“网页授权接口”，
+ * 参考http://mp.weixin.qq.com/wiki/17/c0f37d5704f0b64713d5d2c37b468d75.html）
+ */
+?>
+
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/> 
+    <title>微信支付样例-支付</title>
+    <script type="text/javascript">
+	//调用微信JS api 支付
+	function jsApiCall()
+	{
+		WeixinJSBridge.invoke(
+			'getBrandWCPayRequest',
+			<?php echo $jsApiParameters; ?>,
+			function(res){
+				WeixinJSBridge.log(res.err_msg);
+				alert(res.err_code+res.err_desc+res.err_msg);
+			}
+		);
+	}
+
+	function callpay()
+	{
+		if (typeof WeixinJSBridge == "undefined"){
+		    if( document.addEventListener ){
+		        document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
+		    }else if (document.attachEvent){
+		        document.attachEvent('WeixinJSBridgeReady', jsApiCall); 
+		        document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
+		    }
+		}else{
+		    jsApiCall();
+		}
+	}
+	</script>
+	<script type="text/javascript">
+	//获取共享地址
+	function editAddress()
+	{
+		WeixinJSBridge.invoke(
+			'editAddress',
+			<?php echo $editAddress; ?>,
+			function(res){
+				var value1 = res.proviceFirstStageName;
+				var value2 = res.addressCitySecondStageName;
+				var value3 = res.addressCountiesThirdStageName;
+				var value4 = res.addressDetailInfo;
+				var tel = res.telNumber;
+				
+				alert(value1 + value2 + value3 + value4 + ":" + tel);
+			}
+		);
+	}
+	
+	window.onload = function(){
+		if (typeof WeixinJSBridge == "undefined"){
+		    if( document.addEventListener ){
+		        document.addEventListener('WeixinJSBridgeReady', editAddress, false);
+		    }else if (document.attachEvent){
+		        document.attachEvent('WeixinJSBridgeReady', editAddress); 
+		        document.attachEvent('onWeixinJSBridgeReady', editAddress);
+		    }
+		}else{
+			editAddress();
+		}
+	};
+	
+	</script>
+</head>
+<body>
+    <br/>
+    <font color="#9ACD32"><b>该笔订单支付金额为<span style="color:#f00;font-size:50px">1分</span>钱</b></font><br/><br/>
+	<div align="center">
+		<button style="width:210px; height:50px; border-radius: 15px;background-color:#FE6714; border:0px #FE6714 solid; cursor: pointer;  color:white;  font-size:16px;" type="button" onclick="callpay()" >立即支付</button>
+	</div>
+</body>
+</html>
